7b1b6b7aa83d5d59d633a06bceb4ab4ad43c0673
David Champion <davidchampion@davids-air.attheco.com>
2018-08-17 15:04:13 -0500

    adds buttons, labels , and images for welcome

---
+++ swoosh/swoosh/Views/BorderButton.swift
@@ -0,0 +1,17 @@
+//
+//  BorderButton.swift
+//  swoosh
+//
+//  Created by David Champion on 8/17/18.
+//  Copyright © 2018 David Champion. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
